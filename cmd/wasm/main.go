0754299801b356d65a0561480d2a6fbdf6617c3f
Piotr Pieróg <ppierog@gmail.com>
2024-05-22 13:04:24 +0200

    WASM continue

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -7,71 +7,146 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"time"
-
 	"syscall/js"
+	"time"
 )
 
-type PKeyRepo struct {
-	Key    rsa.PrivateKey
-	Loaded bool
+type Key interface {
+	rsa.PrivateKey | rsa.PublicKey
 }
 
-var pkeyRepo PKeyRepo
+type KeyStorage[T Key] struct {
+	Key          T
+	Loaded       bool
+	MarshaledB64 string
+}
 
-func GenerateKey() (string, error) {
+type KeyNum int
+
+const ( // iota is reset to 0
+	Key1 KeyNum = iota // 0
+	Key2        = iota // 1
+	Key3        = iota // 2
+)
+
+var privRepo KeyStorage[rsa.PrivateKey]
+var pubRepo [3]KeyStorage[rsa.PublicKey]
+
+func GenerateKey() (bool, error) {
+	privRepo.Loaded = false
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		return "", err
-	}
-	marshaledPriv := x509.MarshalPKCS1PrivateKey(privateKey)
-	b64Priv := base64.StdEncoding.EncodeToString(marshaledPriv)
-	return b64Priv, nil
-
-}
-
-func LoadKey(b64Priv string) (bool, error) {
-	marshaledPriv, err := base64.StdEncoding.DecodeString(b64Priv)
-
-	if err != nil {
 		return false, err
 	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(marshaledPriv)
+	privRepo.Key = *privateKey
+	marshaledPriv := x509.MarshalPKCS1PrivateKey(&privRepo.Key)
+	privRepo.MarshaledB64 = base64.StdEncoding.EncodeToString(marshaledPriv)
+	privRepo.Loaded = true
 
-	if err != nil {
-		return false, err
-	}
-	pkeyRepo.Key = *privateKey
-	pkeyRepo.Loaded = true
 	return true, nil
 }
 
-func EncryptPubKey(plainText string) (string, error) {
-	if !pkeyRepo.Loaded {
-		return "", errors.New("PrivateKey not loaded")
+func fetchKey[T Key](repo *KeyStorage[T]) (string, error) {
+	if !repo.Loaded {
+		return "", errors.New("Key not loaded")
 	}
+
+	return repo.MarshaledB64, nil
+}
+
+func loadKey[T Key](b64key string, storage *KeyStorage[T], parser func([]byte) (*T, error)) (bool, error) {
+	storage.Loaded = false
+	marshaled, err := base64.StdEncoding.DecodeString(b64key)
+
+	if err != nil {
+		return false, err
+	}
+	key, err := parser(marshaled)
+
+	if err != nil {
+		return false, err
+	}
+	storage.Key = *key
+	storage.Loaded = true
+	storage.MarshaledB64 = b64key
+	return true, nil
+}
+
+func LoadPrivKey(b64Priv string) (bool, error) {
+	return loadKey(b64Priv, &privRepo, x509.ParsePKCS1PrivateKey)
+}
+
+func FetchPrivKey() (string, error) {
+	return fetchKey(&privRepo)
+}
+
+func LoadPubKey(b64Pub string, num KeyNum) (bool, error) {
+	if num > Key3 {
+		return false, errors.New("Bank Not supported")
+	}
+	return loadKey(b64Pub, &pubRepo[num], x509.ParsePKCS1PublicKey)
+}
+
+func encryptPubKey(plainText string, key *rsa.PublicKey) (string, error) {
+
+	fmt.Println("Before conversion", plainText)
 	msg := []byte(plainText)
-	cipher, err := rsa.EncryptPKCS1v15(rand.Reader, &pkeyRepo.Key.PublicKey, msg)
+	fmt.Println("After conversion", msg)
+	cipher, err := rsa.EncryptPKCS1v15(rand.Reader, key, msg)
 	if err != nil {
 		return "", err
 	}
+	fmt.Println("After encryption", cipher)
 	return base64.StdEncoding.EncodeToString(cipher), nil
 }
 
-func DecryptPrivKey(b64cipher string) (string, error) {
-	if !pkeyRepo.Loaded {
+func EncryptPubKey(plainText string, num KeyNum) (string, error) {
+	if num > Key3 {
+		return "", errors.New("Bank Not supported")
+	}
+	if !pubRepo[num].Loaded {
+		return "", errors.New("Key Not loaded")
+	}
+	return encryptPubKey(plainText, &pubRepo[num].Key)
+
+}
+
+func Encrypt(plainText string) (string, error) {
+	if !privRepo.Loaded {
 		return "", errors.New("PrivateKey not loaded")
 	}
-	cipher, err := base64.StdEncoding.DecodeString(b64cipher)
+	fmt.Println("plain Text  is ", plainText)
+	return encryptPubKey(plainText, &privRepo.Key.PublicKey)
+}
+
+func Decrypt(b64 string) (string, error) {
+	if !privRepo.Loaded {
+		return "", errors.New("PrivateKey not loaded")
+	}
+	cipher, err := base64.StdEncoding.DecodeString(b64)
 
 	if err != nil {
 		return "", err
 	}
-	plainText, err := rsa.DecryptPKCS1v15(rand.Reader, &pkeyRepo.Key, cipher)
+
+	plainText, err := rsa.DecryptPKCS1v15(rand.Reader, &privRepo.Key, cipher)
 	if err != nil {
 		return "", err
 	}
 	return string(plainText), nil
+}
+
+func ToBase64(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func FromBase64(b64 string) ([]byte, error) {
+	cipher, err := base64.StdEncoding.DecodeString(b64)
+
+	if err != nil {
+		return nil, err
+	}
+	return cipher, nil
 }
 
 func JsonWrapper[T any](F func(args []js.Value) (T, error)) js.Func {
@@ -96,11 +171,11 @@ func JsonWrapper[T any](F func(args []js.Value) (T, error)) js.Func {
 func main() {
 
 	now := time.Now()
-	fmt.Println("Go Web Assembly %s", now)
+	fmt.Println("Go Web Assembly ", now)
 
-	js.Global().Set("GenerateKey", JsonWrapper(func(args []js.Value) (string, error) {
+	js.Global().Set("GenerateKey", JsonWrapper(func(args []js.Value) (bool, error) {
 		if len(args) != 0 {
-			return "", errors.New("Invalid no of arguments passed")
+			return false, errors.New("Invalid number of arguments passed")
 		}
 		return GenerateKey()
 
@@ -108,10 +183,49 @@ func main() {
 
 	js.Global().Set("LoadKey", JsonWrapper(func(args []js.Value) (bool, error) {
 		if len(args) != 1 {
-			return false, errors.New("Invalid no of arguments passed")
+			return false, errors.New("Invalid number of arguments passed")
 		}
 		input := args[0].String()
-		return LoadKey(input)
+		return LoadPrivKey(input)
+
+	}))
+
+	js.Global().Set("LoadPubKey", JsonWrapper(func(args []js.Value) (bool, error) {
+		if len(args) != 2 {
+			return false, errors.New("Invalid number of arguments passed")
+		}
+		pubKey := args[0].String()
+		num := args[1].Int()
+		return LoadPubKey(pubKey, KeyNum(num))
+
+	}))
+
+	js.Global().Set("Encrypt", JsonWrapper(func(args []js.Value) (string, error) {
+		if len(args) != 1 {
+			return "", errors.New("Invalid number of arguments passed")
+		}
+		fmt.Println("Arg0 is", args[0])
+		input := args[0].String()
+		fmt.Println("Arg0 is now", input)
+
+		return Encrypt(input)
+
+	}))
+
+	js.Global().Set("Decrypt", JsonWrapper(func(args []js.Value) (string, error) {
+		if len(args) != 1 {
+			return "", errors.New("Invalid number of arguments passed")
+		}
+		input := args[0].String()
+		return Decrypt(input)
+
+	}))
+
+	js.Global().Set("FetchPrivKey", JsonWrapper(func(args []js.Value) (string, error) {
+		if len(args) != 0 {
+			return "", errors.New("Invalid number of arguments passed")
+		}
+		return FetchPrivKey()
 
 	}))
 
